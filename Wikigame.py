bd28c821f9efbeef42081286f540087885f0bcec
pouychanfrederic <frederic.pouchan@epsi.fr>
2020-12-15 09:50:01 +0100

    first commit

---
+++ Wikigame.py
@@ -0,0 +1,5 @@
+print("Bienvenue dans le jeu de Wikipédia ! ")
+print("Le principe est simple, une page de départ et d'arrivée vont être tirée au sort")
+print("A vous d'y arriver le plus vite possible")
+print("Bonne chance !")
+print("salut")
