91951b6acc9932869620691bee30aefcec703161
pouychanfrederic <frederic.pouchan@epsi.fr>
2020-12-26 14:41:11 +0100

    Get nom de la page

--- Wikigame.py
+++ Wikigame.py
@@ -8,14 +8,20 @@ print("Bonne chance !")
 pageDeDepart = 'https://fr.wikipedia.org/wiki/Sp%C3%A9cial:Page_au_hasard'
 pageDArrivee = 'https://fr.wikipedia.org/wiki/Sp%C3%A9cial:Page_au_hasard'
 
+def getHrefFromWikipage():
+    print("test")
+
 with urllib.request.urlopen(pageDeDepart) as response:
     webpage = response.read()
     soup = BeautifulSoup(webpage, 'html.parser')
     pageDeDepart = soup.select("[rel='canonical']")[0]['href']
+    nomDeLaPageDeDepart = soup.find('h1', class_ ='firstHeading')
     print("Vous commencez de la page : " + pageDeDepart)
 
 with urllib.request.urlopen(pageDArrivee) as response:
     webpage = response.read()
     soup = BeautifulSoup(webpage, 'html.parser')
     pageDArrivee = soup.select("[rel='canonical']")[0]['href']
+    nomDeLaPageDArrivee = soup.find('h1', class_ ='firstHeading')
     print("Vous devez arriver à la page : " + pageDArrivee)
+    # print("Le nom de la page est : " + nomDeLaPage.getText())
